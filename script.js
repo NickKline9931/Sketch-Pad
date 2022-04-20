3aeee3d9f3b25cbed9da8ad66e0750f81b4a8c15
Nick <nickkline9931@gmail.com>
2022-04-20 12:00:53 -0400

    Add clear button functionality

---
+++ script.js
@@ -0,0 +1,55 @@
+let color = "black";
+let click = true;
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let amount = size * size;
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = "white";
+        square.addEventListener('mouseover', colorSquare);
+        board.insertAdjacentElement("beforeend", square);
+        };
+    }
+
+
+populateBoard(30);
+
+function changeSize(input) {
+    if(input >=2 && input <=100 ){
+   document.querySelector('.error').style.display = 'none';
+    populateBoard(input);
+} else {
+    document.querySelector('.error').style.display = 'flex';
+    console.log('2-100 only!')
+}}
+
+function colorSquare() {
+    if (click) {
+    this.style.backgroundColor = color;
+}
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function clearBoard(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
+
+document.querySelector('body').addEventListener('click', (e) =>{
+    if (e.target.tagName != 'BUTTON' && e.target.tagName != 'INPUT') {
+    click = !click;
+    if(click) {
+        document.querySelector('.mode').textContent = "Pencil: On";
+    } else {
+        document.querySelector('.mode').textContent = "Pencil: Off";
+    }
+    }
+});
+
