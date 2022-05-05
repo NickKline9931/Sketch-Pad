1d5786499e88ebda9839ebd5d06b7a69f9b06840
Nick <nickkline9931@gmail.com>
2022-05-05 15:26:04 -0400

    Add gold border to selected color

--- script.js
+++ script.js
@@ -1,5 +1,69 @@
 let color = "black";
 let click = true;
+function changeColor(choice) {
+    color = choice;
+    if (color == "black") {
+        document.querySelector('#black').style.borderColor = "gold";
+    } else {
+        document.querySelector('#black').style.borderColor = "black";
+    }
+    
+    if (color == "red") {
+        document.querySelector('#red').style.borderColor = "gold";
+    } else {
+        document.querySelector('#red').style.borderColor = "black";
+    }
+
+    if (color == "white") {
+        document.querySelector('#white').style.borderColor = "gold";
+    } else {
+        document.querySelector('#white').style.borderColor = "black";
+    }
+
+    if (color == "blue") {
+        document.querySelector('#blue').style.borderColor = "gold";
+    } else {
+        document.querySelector('#blue').style.borderColor = "black";
+    }
+
+    if (color == "yellow") {
+        document.querySelector('#yellow').style.borderColor = "gold";
+    } else {
+        document.querySelector('#yellow').style.borderColor = "black";
+    }
+
+    if (color == "#b300b3") {
+        document.querySelector('#purple').style.borderColor = "gold";
+    } else {
+        document.querySelector('#purple').style.borderColor = "black";
+    }
+
+    if (color == "#33ff77") {
+        document.querySelector('#green').style.borderColor = "gold";
+    } else {
+        document.querySelector('#green').style.borderColor = "black";
+    }
+
+    if (color == "orange") {
+        document.querySelector('#orange').style.borderColor = "gold";
+    } else {
+        document.querySelector('#orange').style.borderColor = "black";
+    }
+
+    if (color == "#ff80d5") {
+        document.querySelector('#pink').style.borderColor = "gold";
+    } else {
+        document.querySelector('#pink').style.borderColor = "black";
+    }
+
+    if (color == "#66ffe6") {
+        document.querySelector('#aqua').style.borderColor = "gold";
+    } else {
+        document.querySelector('#aqua').style.borderColor = "black";
+    }
+}    
+
+changeColor('black');
 function populateBoard(size) {
     let board = document.querySelector(".board");
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -32,25 +96,22 @@ function colorSquare() {
 }
 }
 
-function changeColor(choice) {
-    color = choice;
-}
 
 function clearBoard(){
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor = "white");
-}
+}    
 
 document.querySelector('body').addEventListener('click', (e) =>{
     if (e.target.tagName != 'BUTTON' && e.target.tagName != 'INPUT') {
-    click = !click;
+    click = !click;    
     if(click) {
         document.querySelector('.mode').textContent = "Pencil: On";
     } else {
         document.querySelector('.mode').textContent = "Pencil: Off";
+    }    
     }
-    }
-});
+});    
 
 
